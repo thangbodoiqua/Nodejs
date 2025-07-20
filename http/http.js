e68337296f02680aa2fa651079ae259b27173cac
thangbodoiqua <dluy2406@gmail.com>
2025-07-20 12:39:44 +0700

    http module

--- http/http.js
+++ http/http.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({
+    header:" hello world"
+  }));
 }).listen(2000);
--- http/http_url.js
+++ http/http_url.js
@@ -1,6 +1,27 @@
 var http = require('http');
+var url = require('url');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
+  var q = url.parse(req.url, true).query;
+  console.log( url.parse(req.url))
+  // .parse() is used to parse the query string into an object
+  // The second argument 'true' indicates that the query string should be parsed into an object
+  // .query contains the parsed query string (eg: ?name=John&age=30 becomes { name: 'John', age: '30' })
+
+  var contentType = 'text/html';
+
+  if (q.json === 'true') {
+    contentType = 'application/json';
+  }
+
+  res.writeHead(200, {'Content-Type': contentType});
+
+ if (contentType === 'text/html') {
+      res.write('<html><body><h1>Hello, World!</h1></body></html>');
+  } else if (contentType === 'application/json') {
+      res.write(JSON.stringify({ message: "Hello, World!" }));
+  }
   res.end();
+
 }).listen(200);
+
